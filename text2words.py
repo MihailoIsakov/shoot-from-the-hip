dabf7c14961e6d5713ac59031f8c0a887d6935df
Mihailo Isakov <mihailo@protonmail.ch>
2016-05-27 16:52:56 +0200

    Loading unicode, removing symbols, logic from bald_latin

---
+++ text2words.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+
+def load_text_as_unicode(fpath):
+    import codecs
+    f = codecs.open(fpath, 'r', 'utf-8')
+    return f.read()
+
+
+def text_balden(text):
+    """
+    Remove cyrillic comments and balden accents
+    """
+    text = text.lower()
+    comments = text.split('\n')
+    comments = remove_cyrillic_and_accents(comments)
+    return " ".join(comments)
+
+
+def remove_symbols(text):
+    bad_chars = ['\n', '\t', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', '-', '/', ':', ';', '<', '=', '>', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+    punctuation = ['.', ',', '?', '!']
+
+    for char in bad_chars:
+        print "Processing {}".format(char)
+        text = text.replace(char, ' ')
+
+    for char in punctuation:
+        print "Processing {}".format(char)
+        text = text.replace(char, ' ' + char + ' ')
+
+    return text 
+
+
+def remove_cyrillic_comments(comments, labels, print_perc=True):
+    clean_coms = []
+    clean_labels = []
+    cyrillic = set(u"АаБбВвГгДдЂђЕеЖжЗзИиЈјКкЛлЉљМмНнЊњОоПпРрСсТтЋћУуФфХхЦцЧчЏџШш")
+
+    for comment, label in zip(comments, labels):
+        if not bool(set(comment.decode('utf8')).intersection(cyrillic)):
+            clean_coms.append(comment)
+            clean_labels.append(label)
+
+    return clean_coms, clean_labels
+
+
+def remove_serbian_accents(comments):
+    bald = []
+    for comment in comments:
+        bald_comment = (comment.decode('utf8')) \
+            .replace(u"ć", u"c") \
+            .replace(u"č", u"c") \
+            .replace(u"š", u"s") \
+            .replace(u"đ", u"dj") \
+            .replace(u"ž", u"z")
+
+        bald.append(bald_comment)
+
+    return bald
+
+
+def remove_cyrillic_and_accents(comments, labels=None, remove_accents=True):
+    if labels is None:
+        _labels = range(len(comments))
+    else:
+        _labels = labels
+
+    comments, _labels = remove_cyrillic_comments(comments, _labels)
+    if remove_accents:
+        comments = remove_serbian_accents(comments)
+
+    if labels:
+        return comments, _labels
+    else:
+        return comments
+
