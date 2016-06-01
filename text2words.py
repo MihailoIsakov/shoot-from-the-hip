7cfefbb6e3e313dbe77a9f6837d0a8ec3a2d701c
Mihailo Isakov <mihailo@protonmail.ch>
2016-06-01 23:40:58 +0200

    Moved around functions, cleaner module namespace

--- text2words.py
+++ text2words.py
@@ -12,6 +12,7 @@ def balden2file(text, fname):
     Remove cyrillic comments and balden accents
     """
     text = text.lower()
+    text = remove_symbols(text)
     comments = text.split('\n')
     comments = remove_cyrillic_and_accents(comments)
 
@@ -36,35 +37,39 @@ def remove_symbols(text):
     return text 
 
 
-def remove_cyrillic_comments(comments, labels, print_perc=True):
-    clean_coms = []
-    clean_labels = []
-    cyrillic = set(u"АаБбВвГгДдЂђЕеЖжЗзИиЈјКкЛлЉљМмНнЊњОоПпРрСсТтЋћУуФфХхЦцЧчЏџШш")
-
-    for comment, label in zip(comments, labels):
-        if not bool(set(comment).intersection(cyrillic)):
-            clean_coms.append(comment)
-            clean_labels.append(label)
-
-    return clean_coms, clean_labels
-
-
-def remove_serbian_accents(comments):
-    bald = []
-    for comment in comments:
-        bald_comment = (comment) \
-            .replace(u"ć", u"c") \
-            .replace(u"č", u"c") \
-            .replace(u"š", u"s") \
-            .replace(u"đ", u"dj") \
-            .replace(u"ž", u"z")
-
-        bald.append(bald_comment)
-
-    return bald
-
-
 def remove_cyrillic_and_accents(comments, labels=None, remove_accents=True):
+    """
+    Removes any comment containing any cyrillic letter.
+    If labels are provided, removes the coresponding labels.
+    If remove_accents is true, removes accents from letter (ć -> c, ž -> z ...)
+    """
+
+    def remove_cyrillic_comments(comments, labels, print_perc=True):
+        clean_coms = []
+        clean_labels = []
+        cyrillic = set(u"АаБбВвГгДдЂђЕеЖжЗзИиЈјКкЛлЉљМмНнЊњОоПпРрСсТтЋћУуФфХхЦцЧчЏџШш")
+    
+        for comment, label in zip(comments, labels):
+            if not bool(set(comment).intersection(cyrillic)):
+                clean_coms.append(comment)
+                clean_labels.append(label)
+    
+        return clean_coms, clean_labels
+
+    def remove_serbian_accents(comments):
+        bald = []
+        for comment in comments:
+            bald_comment = (comment) \
+                .replace(u"ć", u"c") \
+                .replace(u"č", u"c") \
+                .replace(u"š", u"s") \
+                .replace(u"đ", u"dj") \
+                .replace(u"ž", u"z")
+    
+            bald.append(bald_comment)
+    
+        return bald
+
     if labels is None:
         _labels = range(len(comments))
     else:
